96da40395e6bc2576feba493829c792b4ce3051b
karbovskiy11 <karbovskiy11@gmail.com>
2025-12-10 06:02:16 +0400

    first

--- main.py
+++ main.py
@@ -7,56 +7,56 @@ import  requests
 import lxml
 import  datetime
 
-# def get_date_api():
-#     count = 0
-#
-#     with open('films_list.csv', 'w') as file_csv:
-#         writer = csv.writer(file_csv)
-#         writer.writerow(
-#             ['Название фильма',
-#             'Рейтинг',
-#             'Год',
-#             'Режисёр',
-#              'Ссылка'
-#              ]
-#         )
-#
-#     for i in range(1, 800):
-#
-#         url_api = f'https://zonafilm.ru/api/movies?page={i}'
-#         # url = 'https://zonafilm.ru/api/movies?page=1'
-#
-#         headers = {
-#             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
-#             'accept': '*/*'
-#         }
-#
-#         response = requests.get(url=url_api, headers=headers)
-#         # response_2 = requests.get(url=url, headers=headers)
-#
-#         films = response.json()['data']
-#         films_list = []
-#
-#         if films != 0:
-#
-#             for item in films:
-#                 item_film = item.get('title')
-#                 item_year = item.get('year')
-#                 item_rating = round(item.get('rating'), 2)
-#                 item_directors = item.get('directors')
-#                 item_slug = 'https://zonafilm.ru/movies/' + item.get('slug')
-#
-#                 films_list.append(
-#                     [item_film, item_rating, item_year, item_directors, item_slug]
-#                 )
-#                 count += 1
-#                 print(count)
-#
-#             with open('films_list.csv', 'a') as file_csv:
-#                 writer = csv.writer(file_csv)
-#                 writer.writerows(
-#                     films_list
-#                 )
+def get_date_api():
+    count = 0
+
+    with open('films_list.csv', 'w') as file_csv:
+        writer = csv.writer(file_csv)
+        writer.writerow(
+            ['Название фильма',
+            'Рейтинг',
+            'Год',
+            'Режисёр',
+             'Ссылка'
+             ]
+        )
+
+    for i in range(1, 800):
+
+        url_api = f'https://zonafilm.ru/api/movies?page={i}'
+        # url = 'https://zonafilm.ru/api/movies?page=1'
+
+        headers = {
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
+            'accept': '*/*'
+        }
+
+        response = requests.get(url=url_api, headers=headers)
+        # response_2 = requests.get(url=url, headers=headers)
+
+        films = response.json()['data']
+        films_list = []
+
+        if films != 0:
+
+            for item in films:
+                item_film = item.get('title')
+                item_year = item.get('year')
+                item_rating = round(item.get('rating'), 2)
+                item_directors = item.get('directors')
+                item_slug = 'https://zonafilm.ru/movies/' + item.get('slug')
+
+                films_list.append(
+                    [item_film, item_rating, item_year, item_directors, item_slug]
+                )
+                count += 1
+                print(count)
+
+            with open('films_list.csv', 'a') as file_csv:
+                writer = csv.writer(file_csv)
+                writer.writerows(
+                    films_list
+                )
 
 def get_date_html():
     count = 1
@@ -95,14 +95,20 @@ def get_date_html():
                         all_links
                     )
 
+def get_data():
+    with open('all_links.csv') as file:
+        data = csv.reader(file)
 
+    for i in data:
+        print(i)
 
 
 
 
 def main():
     # get_date_api()
-    get_date_html()
+    # get_date_html()
+    get_data()
 
 if __name__ == '__main__':
     main()
